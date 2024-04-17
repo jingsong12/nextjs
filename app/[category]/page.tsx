68c32f04d0df66a0d2856c312aefd0737e47b3e0
jingsong <304444809@qq.com>
2024-04-17 15:04:05 +0800

    add category detail page

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -1,14 +1,9 @@
 "use client";
 
 import { Navigator } from "@/components/navigator";
-import { data, result } from "@/data";
 import styled from "styled-components";
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
-import { getCategories } from "@/api/data";
+import { useQuery } from "@tanstack/react-query";
+import { getCategories, getCategoryDetail } from "@/api/data";
 import { baseUrl } from "@/lib/common";
 import { Base } from "@/components/common";
 
@@ -18,25 +13,27 @@ const Wrapper = styled.div`
   justify-content: space-around;
 `;
 
-export default function Home() {
+export default function Page({ params }: { params: { category: string } }) {
+  const { category } = params;
   const { data, isLoading, isError } = useQuery({
-    queryFn: async () => await getCategories(),
-    queryKey: ["categories"],
+    queryFn: async () => await getCategoryDetail(category),
+    queryKey: ["categoryDetail"],
   });
 
-  const { results } = data ?? {};
+  const { items } = data ?? {};
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Sorry There was an Error</div>;
+
   return (
     <>
       <Navigator />
       <Wrapper>
-        {results.map(({ name, url }) => (
+        {items.map(({ name, url }) => (
           <Base
             key={name}
             name={name}
-            url={url.split(baseUrl + "/item-category/")[1]}
+            url={"/" + category + "/" + url.split(baseUrl + "/item/")[1]}
           />
         ))}
       </Wrapper>
--- interface/base.ts
+++ interface/base.ts
@@ -1,10 +1,26 @@
-export interface ICategory {
-    name: string;
-    url: string;
+export interface IBase {
+  name: string;
+  url: string;
 }
 
-export interface ICategoriRes{
-    count: number;
-    next: string;
-    results: ICategory[]
+export interface ICategory {
+  name: string;
+  url: string;
+}
+
+export interface ICategoriesRes {
+  count: number;
+  next: string;
+  results: ICategory[];
+}
+
+export interface ICategoryDetail {
+  id: string;
+  items: ICategory[];
+  name: string;
+  names: {
+    language: IBase;
+    name: string;
+  };
+  pocket: IBase;
 }
